c31f6d89da5becfb9beba1d0b3579fb1bf0ffcf4
Chris Wingo and Devin Ludwig <chris@>
2016-11-01 14:17:58 -0700

    add a form input for months of projected interest that returns and displays projected balance and added currency display method to outputs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
-var newAccount = {}
-function BankAccount(userName,initialDeposit,balance) {
+var newAccount = {};
+var checkingInterest = 0.01;
+var savingsInterest = 0.04;
+function BankAccount(userName, accountType, balance) {
   this.userName = userName;
-  this.initialDeposit = parseFloat(initialDeposit);
-  this.balance = parseFloat(initialDeposit);
+  this.accountType = accountType;
+  this.balance = parseFloat(balance);
+  if(accountType === "checking"){
+    this.interestRate = checkingInterest;
+  } else if(accountType === "savings"){
+    this.interestRate = savingsInterest;
+  }
 };
 
 BankAccount.prototype.deposit = function(amount) {
@@ -17,19 +24,32 @@ BankAccount.prototype.withdrawal = function(amount) {
   console.log(this.balance);
 };
 
+BankAccount.prototype.compound = function(months) {
+  var projectedBalance = 0;
+  console.log(months);
+  months = parseInt(months);
+  console.log(months);
+  projectedBalance =  1 + this.interestRate;
+    console.log(projectedBalance);
+  projectedBalance = Math.pow(projectedBalance, months);
+    console.log(projectedBalance);
+  projectedBalance = projectedBalance * this.balance;
+  console.log(projectedBalance);
+    return projectedBalance;
+}
+
 
 //UI Logic
 $(document).ready(function(){
-  var balance = 0;
-  var account = {};
   $("form#new-account").submit(function(event){
     event.preventDefault();
     var name = $("input#name").val();
     var initialDeposit = $("input#initial-deposit").val();
+    var accountType = $("#account-type").val();
     console.log(name, initialDeposit)
-    newAccount = new BankAccount(name, initialDeposit, initialDeposit);
+    newAccount = new BankAccount(name, accountType, initialDeposit);
     console.log(newAccount);
-    $('#balance').text('$' + newAccount.balance);
+    $('#balance').text(newAccount.balance.toLocaleString('en-US', { style: 'currency', currency: 'USD' }));
     // return newAccount;
   });
   $("form#deposit-withdrawal").submit(function(event){
@@ -44,7 +64,11 @@ $(document).ready(function(){
     if (withdrawal != "") {
       newAccount.withdrawal(withdrawal);
     }
-    console.log(newAccount.balance);
-    $('#balance').text('$' + newAccount.balance.toFixed(2));
+    $('#balance').text(newAccount.balance.toLocaleString('en-US', { style: 'currency', currency: 'USD' }));
+  });
+  $("form#projection-form").submit(function(event){
+    event.preventDefault();
+    var months = $("input#months").val();
+    $('#projected').text('$' + newAccount.compound(months).toCurrency());
   });
 });
